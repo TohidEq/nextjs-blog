24d8feb42ac2a997f6942e2738d01a81f1d3997b
TohidEq <TohidEq@gmail.com>
2023-11-03 21:19:54 +0330

    Add @tailwindcss/aspect-ratio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
 export default config;
